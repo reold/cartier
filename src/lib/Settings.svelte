1352161cfaeef515025c2911b3ad67e80d2584d3
reold <redicrafty@gmail.com>
2024-02-10 00:23:15 +0530

    + more settings

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -1,13 +1,43 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import Dialog from "./Dialog.svelte";
+  import { notify } from "../App.svelte";
 
   export let hidden: boolean;
+  let settings = {};
 
-  let settings = {
-    rsaved_userid: localStorage.getItem("cartier-userid") || "not saved",
-    rserver_url: window["cartier-server-url"],
-    wthank_you_for: "using this app during it's beta days!",
+  const purgeOpfs = () => {
+    notify(
+      "deleting all downloads and refreshing",
+      true,
+      10,
+      async (destroy) => {
+        const root = await navigator.storage.getDirectory();
+        await root.remove({ recursive: true });
+        destroy();
+        window.location.reload();
+      }
+    );
   };
+
+  const loadSettings = () => {
+    notify("loading settings", true, 5, async (destroy) => {
+      const opfsEstimate = await navigator.storage.estimate();
+      let opfsUsed = 0;
+
+      if (opfsEstimate) opfsUsed = opfsEstimate["usage"] / 1024 ** 2;
+
+      settings = {
+        rsaved_userid: localStorage.getItem("cartier-userid") || "not saved",
+        rserver_url: window["cartier-server-url"],
+        ropfs_used: `${opfsUsed.toFixed(1)} MB`,
+      };
+
+      destroy();
+    });
+  };
+
+  onMount(loadSettings);
 </script>
 
 <Dialog {hidden}>
@@ -65,4 +95,54 @@
       {/each}
     </tbody>
   </table>
+  <div class="grid grid-cols-10 gap-1 mt-2 btns-tab font-bold">
+    <button
+      class="bg-blue-500 text-black svg-icon col-span-3"
+      on:click={loadSettings}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-[1em]"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+        />
+      </svg>
+      <p>refresh</p>
+    </button>
+    <button
+      class="bg-red-500 text-white svg-icon col-span-5"
+      on:click={purgeOpfs}
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="w-[1em]"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <p class="">delete downloads</p></button
+    >
+    <button class="col-span-2" on:click={() => (hidden = true)}>close</button>
+  </div>
 </Dialog>
+
+<style>
+  * {
+    @apply select-text;
+  }
+
+  .btns-tab button {
+    @apply p-0.5 py-2 text-sm;
+  }
+</style>
